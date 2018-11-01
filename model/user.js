4d2faf9ea4f0e024aba6bf74522fd6ebda7ea733
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-01 13:48:05 -0600

    added a user Schema

---
+++ model/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        first: {
+            type: String,
+            required: true
+        },
+        last:  {
+            type: String,
+            required: true
+        },
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: Boolean,
+    location: String
+});
+
+
+module.exports = mongoose.model("User", userSchema);
