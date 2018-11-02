02dc4a5142443278233b21e17781d74f79d65217
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-02 17:59:14 -0600

    added a dailytime model

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,53 +1,53 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Signup extends Component {
-    constructor() {
-        super();
-        this.state = {
-            inputs: {
-                username: '',
-                password: '',
-                name: {
-                    first: '',
-                    last: ''
-                }
-            }
-        }
-    }
+// export default class Signup extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             inputs: {
+//                 username: '',
+//                 password: '',
+//                 name: {
+//                     first: '',
+//                     last: ''
+//                 }
+//             }
+//         }
+//     }
 
-    handleChange = (e) => {
-        e.persist();
-        this.setState(prevState => {
-            return {
-                inputs: {
-                    ...prevState.inputs,
-                    [e.target.name]: e.target.value
-                }
-            }
-        })
-    }
+//     handleChange = (e) => {
+//         e.persist();
+//         this.setState(prevState => {
+//             return {
+//                 inputs: {
+//                     ...prevState.inputs,
+//                     [e.target.name]: e.target.value
+//                 }
+//             }
+//         })
+//     }
 
-    clearInputs() {
-        this.setState({
-            inputs: {
-                name: '',
-                username: '',
-                password: ''
-            }
-        })
-    }
+//     clearInputs() {
+//         this.setState({
+//             inputs: {
+//                 name: '',
+//                 username: '',
+//                 password: ''
+//             }
+//         })
+//     }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.signup(this.state.inputs);
-        this.clearInputs();
-    }
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+//         this.props.signup(this.state.inputs);
+//         this.clearInputs();
+//     }
 
-    render() {
-        return (
-            <div>
+//     render() {
+//         return (
+//             <div>
                 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
