99ed2669258855db90305a83e4db7751ec9565f4
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-01 18:18:07 -0600

    added signup guts

--- client/src/Components/Signup.js
+++ client/src/Components/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Signup() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class Signup extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputs: {
+                username: '',
+                password: '',
+                name: {
+                    first: '',
+                    last: ''
+                }
+            }
+        }
+    }
+
+    handleChange = (e) => {
+        e.persist();
+        this.setState(prevState => {
+            return {
+                inputs: {
+                    ...prevState.inputs,
+                    [e.target.name]: e.target.value
+                }
+            }
+        })
+    }
+
+    clearInputs() {
+        this.setState({
+            inputs: {
+                name: '',
+                username: '',
+                password: ''
+            }
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.signup(this.state.inputs);
+        this.clearInputs();
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
-
-export default Signup
