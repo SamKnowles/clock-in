376f93f354eb9e8bdb2840c2d0d24c5af54ebffe
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-12 16:20:37 -0700

    created the model

--- model/user.js
+++ model/user.js
@@ -2,40 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    name: {
-        first: {
+    // userId: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "User",
+    //     required: true
+    // },
+    inputs: {
+        firstName: {
             type: String,
             required: true
         },
-        last: {
+        lastName: {
             type: String,
             required: true
         },
     },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    admin: Boolean,
-    location: String
+    // username: {
+    //     type: String,
+    //     required: true,
+    //     unique: true,
+    //     lowercase: true
+    // },
+    // password: {
+    //     type: String,
+    //     required: true
+    // },
+    // admin: Boolean,
+    // location: String
 });
 
-userSchema.methods.withoutPassword = function () {
-    let user = this.toObject();
-    delete user.password;
-    return user;
-}
+// userSchema.methods.withoutPassword = function () {
+//     let user = this.toObject();
+//     delete user.password;
+//     return user;
+// }
 
 
 module.exports = mongoose.model('user', userSchema);
